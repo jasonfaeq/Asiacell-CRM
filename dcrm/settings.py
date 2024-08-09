a8389ec2bcb04d24a6ab03d16118ef1ad8b51878
Jason Faeq <jasonfaeq@gmail.com>
2024-08-09 03:03:17 +0200

    added requirements.txt and updated settings.py

--- dcrm/settings.py
+++ dcrm/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = 'django-insecure-3)sgs-*5ph7itcyeew+qvi&g0q8=kwmwqbaym0@7h49pm6ldmn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['myCRMDjangoApplication.azurewebsites.net']
 
 
 # Application definition
